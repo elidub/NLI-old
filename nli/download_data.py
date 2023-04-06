1d8b3041ce2a86870abed787880869db6e4ed9ab
Elias Dubbeldam <eliasdubbeldam@gmail.com>
2023-04-06 12:12:31 +0200

    preprocessing

--- nli/download_data.py
+++ nli/download_data.py
@@ -3,10 +3,22 @@ import zipfile
 import os
 from datasets import load_dataset
 
+import nltk
+nltk.download('punkt')
+
+
+def preprocess(example):
+    example['premise']    = [word.lower() for word in nltk.tokenize.word_tokenize(example['premise'])]
+    example['hypothesis'] = [word.lower() for word in nltk.tokenize.word_tokenize(example['hypothesis'])]
+    return example
 
 def download_snli():
     print('Downloading SNLI dataset...')
     dataset_snli = load_dataset("snli")
+
+    print('Pre-processing SNLI dataset...')
+    for split in dataset_snli:
+        dataset_snli[split] = dataset_snli[split].map(preprocess)
 
     print('Saving SNLI dataset to disk...')
     dataset_snli.save_to_disk("data/snli")
@@ -35,4 +47,4 @@ if __name__ == '__main__':
     os.makedirs('data', exist_ok=True)
 
     download_snli()
-    download_glove()
+    # download_glove()
